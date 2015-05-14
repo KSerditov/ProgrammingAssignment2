9abe50d6b730aa6f4a27144d61ba9a2857c07643
Konstantin Serditov <serditov@gmail.com>
2015-05-14 16:39:20 +0300

    KSerditov. Submit code for Coursera course Programming Assignment 2: Lexical Scoping.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,41 @@
 
 ## Write a short comment describing this function
 
+## Similar to the caching vector mean example, this function creates 
+## a list with functions to set and get original matrix and also to set and 
+## get its inverse.
 makeCacheMatrix <- function(x = matrix()) {
 
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    
+    list(set = set, 
+         get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
+## This function works with "CacheMatrix" list created with the function above
+## and checks if inverse matrix was already calculated
+## to avoid unnecessary calculation.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
